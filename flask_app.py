c9b182e9a8037b279df243534777f4212becaecf
Vyacheslav Klevchenya <klevchenia.viacheslav@outlook.com>
2019-12-05 01:11:51 +0200

    Change blos list view

--- flask_app.py
+++ flask_app.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
         print(url_for('login'))
         print(url_for('login', next='/'))
         print(url_for('profile', user_id='John Doe'))   
-    # app.run(debug=True)
+    app.run(debug=True)
     # Alt. runner properly for Code, where there is no need to use the in-browser debugger or the reloader
-    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
+    # app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
