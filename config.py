d3bd0699c802671dd46584a74fa8d39d44fa720a
Viachesalv Klevchenia <viacheslav.klevchenia@paradoxinteractive.com>
2019-12-04 16:41:45 +0200

    PAW configuration stored

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 
 class Config(object):
     # Database connection (hardcoded becasue doesn't worked good as variables)
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://newuser:BUSDEVGjtx$mGn3@6L79@127.0.0.1:3306/course_work"
+    # SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://newuser:BUSDEVGjtx$mGn3@6L79@127.0.0.1:3306/course_work"
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://viacheslav:#$KLag8*0V9DAx*i80kp@viacheslav.mysql.pythonanywhere-services.com/viacheslav$course_work"
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
