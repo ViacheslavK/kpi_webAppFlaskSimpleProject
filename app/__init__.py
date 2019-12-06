acfad9597f8eb382f21c17cd44f9b90425b9065f
Viachesalv Klevchenia <viacheslav.klevchenia@paradoxinteractive.com>
2019-12-06 14:23:36 +0200

    ?

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__, static_folder=f"{url_for('static')}", template_folder=f"{url_for('templates')}")
 app.config.from_object(Config)
 app.secret_key = "something_only_you_know"
 
